7c82912f666668c56cee22dcb4e2913c65ac6551
mtgto <hogerappa@gmail.com>
2020-10-18 14:06:34 +0900

    Remove main window from Window menu list

--- Fomalhaut2/AppDelegate.swift
+++ Fomalhaut2/AppDelegate.swift
@@ -9,7 +9,9 @@ let log = XCGLogger.default
 class AppDelegate: NSObject, NSApplicationDelegate {
 
   func applicationDidFinishLaunching(_ aNotification: Notification) {
-    // Insert code here to initialize your application
+    // Remove main window from window list of Window menu
+    // Setting isExcludedFromWindowsMenu in BookWindowController#viewDidLoad is ignored...
+    NSApp.windows.first?.isExcludedFromWindowsMenu = true
   }
 
   func applicationWillTerminate(_ aNotification: Notification) {
@@ -25,4 +27,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     return true
   }
 
+  @IBAction func showLibraryWindow(_ sender: Any) {
+    NSApp.windows.first?.makeKeyAndOrderFront(sender)
+  }
 }

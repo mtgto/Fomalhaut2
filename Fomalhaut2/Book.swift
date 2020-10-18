1e5fb6733b8cb959bebe1a71bbd92e14a2db3d42
mtgto <hogerappa@gmail.com>
2020-10-18 13:27:27 +0900

    Add UUID to Book for primary key

--- Fomalhaut2/Book.swift
+++ Fomalhaut2/Book.swift
@@ -4,11 +4,16 @@ import Cocoa
 import RealmSwift
 
 class Book: Object {
+  @objc dynamic var id: String = UUID().uuidString
   @objc dynamic var filename: String = ""
   @objc dynamic var bookmark: Data = Data()
   @objc dynamic var readCount: Int = 0
   @objc dynamic var lastPageIndex: Int = 0
   @objc dynamic var createdAt: Date = Date()
+
+  override static func primaryKey() -> String? {
+    return "id"
+  }
 
   func resolveURL(bookmarkDataIsStale: inout Bool) throws -> URL {
     return try URL(

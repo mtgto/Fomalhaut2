514e1eaf9c79b39449af6212ad40dd53014f62d7
mtgto <hogerappa@gmail.com>
2020-10-29 19:02:18 +0900

    Use multithreads and ImageCache for PdfDocument

--- Fomalhaut2/PdfDocument.swift
+++ Fomalhaut2/PdfDocument.swift
@@ -10,10 +10,16 @@ enum PdfError: Error {
 class PdfDocument: BookDocument {
   static let UTI: String = "com.adobe.pdf"
   private var pdfDocument: PDFDocument?
+  private var url: URL?
+  private let operationQueue: OperationQueue = {
+    let queue = OperationQueue()
+    return queue
+  }()
 
   override func read(from url: URL, ofType typeName: String) throws {
     if let pdf = PDFDocument(url: url) {
       self.pdfDocument = pdf
+      self.url = url
     } else {
       throw NSError(domain: "net.mtgto.Fomalhaut2", code: 0, userInfo: nil)
     }
@@ -26,21 +32,29 @@ extension PdfDocument: BookAccessible {
   }
 
   func image(at page: Int, completion: @escaping (Result<NSImage, Error>) -> Void) {
-    if let pdfPage = self.pdfDocument!.page(at: page), let data = pdfPage.dataRepresentation,
-      let image = NSImage(data: data)
-    {
-      log.debug("pdf image size \(image.size.width)x\(image.size.height)")
-      if page == 0 && self.book!.thumbnailData == nil {
-        do {
-          try self.setBookThumbnail(image)
-        } catch {
-          log.error("Error while creating thumbnail: \(error)")
-        }
-      }
+    let imageCacheKey = ImageCacheKey(archiveURL: self.url!, pageIndex: page)
+    if let image = imageCache.object(forKey: imageCacheKey) {
+      log.debug("success to load from cache at \(page)")
       completion(.success(image))
-    } else {
-      log.info("Error while extracting at \(page)")
-      completion(.failure(PdfError.unknown))
+      return
+    }
+    self.operationQueue.addOperation {
+      if let pdfPage = self.pdfDocument!.page(at: page), let data = pdfPage.dataRepresentation,
+        let image = NSImage(data: data)
+      {
+        //log.debug("pdf image size \(image.size.width)x\(image.size.height)")
+        if page == 0 && self.book!.thumbnailData == nil {
+          do {
+            try self.setBookThumbnail(image)
+          } catch {
+            log.error("Error while creating thumbnail: \(error)")
+          }
+        }
+        completion(.success(image))
+      } else {
+        log.info("Error while extracting at \(page)")
+        completion(.failure(PdfError.unknown))
+      }
     }
   }
 

4a241068f76bcb5542d25a14c317867dec3f5e94
mtgto <hogerappa@gmail.com>
2021-01-31 16:47:11 +0900

    Add localizedDescription to BookAccessibleError

--- Fomalhaut2/BookAccessibleError.swift
+++ Fomalhaut2/BookAccessibleError.swift
@@ -1,7 +1,20 @@
 // SPDX-FileCopyrightText: 2020 mtgto <hogerappa@gmail.com>
 // SPDX-License-Identifier: GPL-3.0-only
 
+import Foundation
+
 enum BookAccessibleError: Error {
   case brokenFile
   case encrypted
+}
+
+extension BookAccessibleError: LocalizedError {
+  var errorDescription: String? {
+    switch self {
+    case .brokenFile:
+      return NSLocalizedString("ErrorBrokenFile", comment: "File is corrupt or unsupported file type")
+    case .encrypted:
+      return NSLocalizedString("ErrorEncrypted", comment: "File is encrypted")
+    }
+  }
 }

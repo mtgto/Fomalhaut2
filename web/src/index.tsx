29ac60b67854ba0fef14a7ac517626b1904aaca7
mtgto <hogerappa@gmail.com>
2021-05-08 13:41:06 +0900

    Use React.StrictMode

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,16 +4,19 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
 import NoSsr from "@material-ui/core/NoSsr";
 import { ThemeProvider } from "@material-ui/core/styles";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import theme from "./theme";
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <NoSsr>
-      <App />
-    </NoSsr>
-  </ThemeProvider>,
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NoSsr>
+        <App />
+      </NoSsr>
+    </ThemeProvider>
+  </StrictMode>,
   document.getElementById("root")
 );

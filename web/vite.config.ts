f71b5e911a600cee9a6e96604eaa089ce5c6c36f
mtgto <hogerappa@gmail.com>
2023-06-29 12:28:02 +0900

    Update web packages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,10 +7,10 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://127.0.0.1:8080",
       },
       "/images": {
-        target: "http://localhost:8080",
+        target: "http://127.0.0.1:8080",
       },
     },
   },
